dbc3a72962d4395228689b3eea7889e5c8499ddb
abfalex <87718997+abfalex@users.noreply.github.com>
2024-05-28 23:03:27 +0300

    Updated functionality

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ import shutil
 import os
 import argparse
 import requests
+import logging
 
-def get_response(url, params):
-	response = requests.get(url, params)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+
+def get_response(session, url, params):
+	response = session.get(url, params=params)
 	response.raise_for_status()
-	print(response.url)
 	return response.json()
 
 
-def download_content(folder, filename, url):
+def download_content(session, folder, filename, url):
 	os.makedirs(folder, exist_ok=True)
 	content_path = os.path.join(folder, filename)
-	picture_bytes = requests.get(picture_link).content
-	with open(f"{content_path}", "wb") as f:
-		f.write(picture_bytes)
+	
+	with session.get(url, stream=True) as response:
+		response.raise_for_status()
+		with open(f"{content_path}", "wb") as f:
+			shutil.copyfileobj(response.raw, f)
+
+		logging.info(f"Downloaded picture to {content_path}")
 
 
 if __name__ == "__main__":
@@ -43,9 +50,14 @@ if __name__ == "__main__":
 	if os.path.isdir(folder):
 		shutil.rmtree(folder)
 
-	for response in range(pictures_count):
-		params = {"filter": "mp4,webm"}
-		picture_link = get_response("https://random.dog/woof.json", params).get('url')
-		link, picture_extension = os.path.splitext(picture_link)
-		filename = f"dog_{response + 1}{picture_extension}"
-		download_content(folder, filename, picture_link)
+	with requests.Session() as session:
+		for response in range(pictures_count):
+			params = {"filter": "mp4,webm"}
+			url = "https://random.dog/woof.json"
+			try:
+				picture_link = get_response(session, url, params).get('url')
+				link, picture_extension = os.path.splitext(picture_link)
+				filename = f"dog_{response + 1}{picture_extension}"
+				download_content(session, folder, filename, picture_link)
+			except requests.RequestException as e:
+				logging.error(f"Failed to download {url}: {e}")
